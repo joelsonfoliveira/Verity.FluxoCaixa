ad9cb25dcdad0f686d17c6d63381506cd72fc89b
JOELSON FREITAS DE OLIVEIRA <joelsonfoliveira@gmail.com>
2026-08-25 04:36:18 -0300

    Adiciona migrations

---
+++ src/Verity.FluxoCaixa.Infraestrutura/Persistencia/Migracoes/20260825071553_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Verity.FluxoCaixa.Infraestrutura.Persistencia.Migracoes
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lancamentos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Data = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Tipo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lancamentos", x => x.Id);
+                }
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lancamentos_Data",
+                table: "Lancamentos",
+                column: "Data"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Lancamentos");
+        }
+    }
+}
